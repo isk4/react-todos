017ff16b5995f2c82096bfc57c315ab82c5cf85d
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2021-01-21 15:18:51 -0300

    Delete unnecessary imports in functional components

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ToDos from '../ToDos';
 
 class Board extends React.Component {
--- src/components/ToDos/index.js
+++ src/components/ToDos/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const ToDos = (props) => {
   const mappedToDos = props.todos.map(todo =>
